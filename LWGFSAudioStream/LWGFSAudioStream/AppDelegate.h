19aeb292e538b8999af31c85b99e97b08d0aa04a
lwg123 <656442711@qq.com>
2017-02-10 13:43:00 +0800

    初始化

---
+++ LWGFSAudioStream/LWGFSAudioStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWGFSAudioStream
+//
+//  Created by weiguang on 2017/2/10.
+//  Copyright © 2017年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWGFSAudioStream/LWGFSAudioStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LWGFSAudioStream
+//
+//  Created by weiguang on 2017/2/10.
+//  Copyright © 2017年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
